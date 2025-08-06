bf1b21f73013f959cd9d9b828ce868af9caad9bd
김민균 <peter_kim@gimmingyun-ui-MacBookAir.local>
2025-08-07 00:59:36 +0900

    Feat:Initial Setting

---
+++ src/main/java/com/uniper/uniper/UniperApplication.java
@@ -0,0 +1,13 @@
+package com.uniper.uniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniper/uniper/UniperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniper.uniper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
